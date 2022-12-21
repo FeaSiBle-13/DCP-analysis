0f7fe994f620f46a08b6de6008882ec4e1471e71
FeaSiBle-13 <120687807+FeaSiBle-13@users.noreply.github.com>
2022-12-21 09:27:59 +0100

    Update saddelpoint_caclulation.py

--- saddelpoint_caclulation.py
+++ saddelpoint_caclulation.py
@@ -116,58 +116,9 @@ for trajectory in range(1, count+1):
                 if words[4] == 'Infinity':
                     infty = True
 
-    #calculates the minimum
     if found and not infty:
         
-        #makes folders
-        mkdir(f'trajectory-{trajectory}')
-        mkdir(f'trajectory-{trajectory}/result')
-        cp(f'trajectory-start/result/cluster_start-out.yml', f'trajectory-{trajectory}/result')
-        mkdir(f'trajectory-{trajectory}/minimum')
-        for obj in ls('trajectory-source/minimum'):
-            cp(f'trajectory-source/minimum/{obj}', f'trajectory-{trajectory}/minimum')
-
-        #reads out the coordinates of the starting minimum
-        n_elecs = reading_n_elecs()
-        R_max = ending_maximum(trajectory)
-
-        #creates ami file for Amolqc
-        with open(f'trajectory-{trajectory}/minimum/stedes.ami', 'w') as sted_file:
-            sted_file.write(f'''! seed for random number generation, not important
-$gen(seed=101)
-! reading the wave function file
-$wf(read,file='{name}.wf')
-$init_rawdata_generation()
-$init_max_search(
-step_size=0.1,
-correction_mode=cut,
-singularity_threshold=0.001,
-method=steepest_descent,
-verbose=2,
-negative_eigenvalues=0,
-eigenvalue_threshold=1e-10)
-! setting the initial position
-$init_walker(
-free
-''')
-            for s in range(n_elecs):
-                for t in R_max[s*3:s*3+3]:
-                    sted_file.write(f'{t} ')
-                sted_file.write('\n')
-            sted_file.write(''')
-$sample(create, size=1, single_point)
-! maximize the walker
-$maximize_sample()''')
-
-        #makes the amolqc run for the minimum single point calculation
-        with cd(f'trajectory-{trajectory}/minimum'):
-            run('amolqc stedes.ami')
-            run('ProcessMaxima cluster.yml')
-            cp('cluster-out.yml', '../result/cluster_end-out.yml')
-
-
-
-#calculates saddlepoints with method input
+    #calculates saddlepoints with method input
         #makes folders
         mkdir(f'trajectory-{trajectory}/DCP_{method}')
         for obj in ls(f'trajectory-source/DCP'):
