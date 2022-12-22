8d611f3faf8a000aa3c505fb6d4adaa85c972e13
FeaSiBle-13 <120687807+FeaSiBle-13@users.noreply.github.com>
2022-12-22 16:01:40 +0100

    Update method_compare.py

--- method_compare.py
+++ method_compare.py
@@ -1,6 +1,7 @@
 #!/bin/env python3
 
 from pyscript import *
+import re
 
 with open(f'DCP-analysis_newton.csv', 'r') as reffile:
     line = reffile.readline()
@@ -8,7 +9,8 @@ with open(f'DCP-analysis_newton.csv', 'r') as reffile:
     for line in reffile:
         words = line.split('\t')
         trajectories = words[4].split(',')
-        for trajectory in trajectories:
+        for item in trajectories:
+            trajectory = re.search(r'\d+', item).group()
             print(f'traj = {trajectory}  pot = {words[1]}')
         
         
