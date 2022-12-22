36f6e00c6d16dc3c0fe0d14ecfc09b438a89613d
FeaSiBle-13 <120687807+FeaSiBle-13@users.noreply.github.com>
2022-12-22 15:14:42 +0100

    Update method_compare.py

--- method_compare.py
+++ method_compare.py
@@ -6,7 +6,7 @@ with open(f'DCP-analysis_newton.csv', 'r') as reffile:
     line = reffile.readline()
     line = reffile.readline()
     for line in reffile:
-        words = line.split()
+        words = line.split('\t')
         print(f'traj = {words[3]}  pot = {words[1]}')
         
     
