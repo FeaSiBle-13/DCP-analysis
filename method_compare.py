956a219a66be7a00a83f3665a40b760d275d604f
FeaSiBle-13 <120687807+FeaSiBle-13@users.noreply.github.com>
2022-12-22 15:09:10 +0100

    Update method_compare.py

--- method_compare.py
+++ method_compare.py
@@ -1,1 +1,11 @@
+from pyscript import *
 
+with open(f'DCP-analysis_newton', 'r') as reffile:
+    line = reffile.readline()
+    line = reffile.readline()
+    for line in reffile:
+        words = line.split()
+        print(f'traj = {words[3]}  pot = {words[1]}')
+        
+    
+        
