fcc39a4713f9a773d0e7d33881d87c2f50f15e46
FeaSiBle-13 <120687807+FeaSiBle-13@users.noreply.github.com>
2022-12-26 12:23:33 +0100

    Update method_compare.py

--- method_compare.py
+++ method_compare.py
@@ -17,7 +17,7 @@ with open(f'DCP-analysis_newton.csv', 'r') as reffile:
         trajectories = words[4].split(',')
         for item in trajectories:
             trajectory = re.search(r'\d+', item).group()
-            potential = words[2]
+            potential = float(words[2])
             list_trajectory.append(trajectory)
             list_potential.append(potential)
 
@@ -29,7 +29,7 @@ with open(f'DCP-analysis_gradient_norm.csv', 'r') as reffile:
         trajectories = words[4].split(',')
         for item in trajectories:
             trajectory = re.search(r'\d+', item).group()
-            potential = words[2]
+            potential = float(words[2])
             for i_item, item in enumerate(list_trajectory):
                 if item == trajectory:
                     if potential != list_potential[i_item]:
