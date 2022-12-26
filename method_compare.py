a7c7bbe434b42145b8dc7b5d50a2d6500100b014
FeaSiBle-13 <120687807+FeaSiBle-13@users.noreply.github.com>
2022-12-26 16:46:10 +0100

    Update method_compare.py

--- method_compare.py
+++ method_compare.py
@@ -47,6 +47,9 @@ for i_item, item in enumerate(list_potential_newton):
         list_stat_potential_newton.append(item)
         list_stat_potential_gradient.append(list_potential_gradient[i_item])
         list_statistic.append(1)
+        print(list_statistic)
+        print(list_stat_potential_newton)
+        print(list_stat_potential_gradient)
         empty = True
     if not empty:
         for i_element, element in enumerate(list_stat_potential_newton):
