c2d648cd862848c24e66f07ef8fef1fab74b3eb4
FeaSiBle-13 <120687807+FeaSiBle-13@users.noreply.github.com>
2022-12-26 12:21:21 +0100

    Update method_compare.py

--- method_compare.py
+++ method_compare.py
@@ -54,7 +54,7 @@ for i_item, item in enumerate(list_potential_newton):
                 list_statistic[item] += 1
             else:
                 list_stat_potential_newton.append(item)
-                list_stat_potential_gradient.append(list_stat_potential[element])
+                list_stat_potential_gradient.append(list_potential_gradient[i_item])
                 list_statistic.append(1)
             
 
