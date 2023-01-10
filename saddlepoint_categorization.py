6d29e2c84d4c4ec463595b9d5abf0fe7bf852665
Felix Bröcker <120687807+FeaSiBle-13@users.noreply.github.com>
2023-01-10 11:25:30 +0100

    Update saddlepoint_categorization.py

--- saddlepoint_categorization.py
+++ saddlepoint_categorization.py
@@ -20,12 +20,17 @@ with open(f'DCP-analysis_{method}.csv', 'r') as reffile:
     list_frequency = []
     list_label_temp = []
     list_frequency_temp = []
+    list_adjacent = []
     line = reffile.readline()
     line = reffile.readline()
     for line in reffile:
         words = line.split('\t')
         list_barrier.append(words[2])
         list_frequency.append(int(words[1]))
+        if words[5] == 'adjacent_minima':
+            list_adjacent.append(True)
+        else:
+            list_adjacent.append(False)
         if 'none' in line:
             list_label_temp.append(words[0])
             list_frequency_temp.append(int(words[1]))            
@@ -46,33 +51,55 @@ with open('assignment_graph.in', 'r') as reffile:
 
 #sums up the frequencies into the categories from the input file
 list_category_frequency = []
+list_category_frequency_adj = []
+list_category_frequency_not_adj = []
 
 for _ in list_category:
     list_category_frequency.append(0)
+    list_category_frequency_adj.append(0)
+    list_category_frequency_not_adj.append(0)
 list_label.append('other_DCPs')
 list_category_frequency.append(0)
+list_category_frequency_adj.append(0)
+list_category_frequency_not_adj.append(0)
     
 for i_barrier, barrier in enumerate(list_barrier):
     if barrier not in list_category and barrier != 'none':
         list_category_frequency[-1] += list_frequency[i_barrier]
+        if list_adjacent[i_barrier]:
+            list_category_frequency_adj[i_category] += list_frequency[i_barrier]
+        else:
+            list_category_frequency_not_adj[i_category] += list_frequency[i_barrier]
     else:    
         for i_category, category in enumerate(list_category):
             if barrier == category:
                 list_category_frequency[i_category] += list_frequency[i_barrier]
+                if list_adjacent[i_barrier]:
+                    list_category_frequency_adj[i_category] += list_frequency[i_barrier]
+                else:
+                    list_category_frequency_not_adj[i_category] += list_frequency[i_barrier]
                 break
        
 list_label += list_label_temp
-list_category_frequency += list_frequency_temp            
+list_category_frequency += list_frequency_temp
+list_category_frequency_adj += list_frequency_temp
+list_category_frequency_not_adj += list_frequency_temp
             
 #sums up the barriers which are slightly different, but are set equal in the input file
 list_final_label = []
 list_final_frequencies = []
+list_final_frequencies_adj = []
+list_final_frequencies_no_adj = []
 for i_label, label in enumerate(list_label):
     if label not in list_final_label:
         list_final_label.append(label)
         list_final_frequencies.append(list_category_frequency[i_label])
+        list_final_frequencies_adj.append(list_category_frequency_adj[i_label])
+        list_final_frequencies_not_adj.append(list_category_frequency_not_adj[i_label])
     else:
         list_final_frequencies[list_final_label.index(label)] += list_category_frequency[i_label]
+        list_final_frequencies_adj[list_final_label.index(label)] += list_category_frequency_adj[i_label]
+        list_final_frequencies_not_adj[list_final_label.index(label)] += list_category_frequency_adj[i_label]
 
 #relabels import names from .csv
 list_final_label[list_final_label.index('walked from basin to infty')] = 'to_infty'
@@ -89,23 +116,29 @@ with open (f'assignment_graph_{method}.out', 'w') as printfile:
     printfile.write('frequency: ')
     for item in list_final_frequencies: 
         printfile.write(f'{item} ')
+    printfile.write('\n')
+    for item in list_final_frequencies_adj: 
+        printfile.write(f'{item} ')
+    printfile.write('\n')
+    for item in list_final_frequencies_not_adj: 
+        printfile.write(f'{item} ')
 print(f'assignment_graph_{method}.out was generated') 
 
 #makes bar diagram
-plt.rcParams['figure.figsize'] = (10, 8.5)
-plt.rcParams['font.size'] = 14
-plt.rcParams['lines.linewidth'] = 2
-plt.rc ('axes', titlesize = 20)
-plt.rc ('axes', labelsize = 18)
-plt.rc ('xtick', labelsize = 10) 
-
-plt.ylabel('frequency', labelpad = 15) 
-
-x_values = range(len(list_final_frequencies))
-
-plt.xticks(x_values, list_final_label)  
-
-plt.bar(x_values, list_final_frequencies, width = 0.5, label = list_label)
-
-plt.savefig(f'assignment_graph_{method}.png')
-print(f'assignment_graph_{method}.png was generated')
+#plt.rcParams['figure.figsize'] = (10, 8.5)
+#plt.rcParams['font.size'] = 14
+#plt.rcParams['lines.linewidth'] = 2
+#plt.rc ('axes', titlesize = 20)
+#plt.rc ('axes', labelsize = 18)
+#plt.rc ('xtick', labelsize = 10) 
+#
+#plt.ylabel('frequency', labelpad = 15) 
+#
+#x_values = range(len(list_final_frequencies))
+#
+#plt.xticks(x_values, list_final_label)  
+#
+#plt.bar(x_values, list_final_frequencies, width = 0.5, label = list_label)
+#
+#plt.savefig(f'assignment_graph_{method}.png')
+#print(f'assignment_graph_{method}.png was generated')
