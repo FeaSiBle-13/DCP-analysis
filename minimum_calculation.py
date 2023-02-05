08a08f1f3cf1efa49b918d94912b9bf7595e0937
Felix Bröcker <120687807+FeaSiBle-13@users.noreply.github.com>
2023-02-05 12:01:29 +0100

    Update minimum_calculation.py

--- minimum_calculation.py
+++ minimum_calculation.py
@@ -22,7 +22,7 @@ def read_ref_file(reffile, coordinate_position):
     with open(f'trajectory-{trajectory}-{reffile}.ref', 'r') as reffile:
         R = []
         line = reffile.readline()
-        while f'{cooridnate_position} F({reffile.upper()}):' not in line:
+        while f'{coordinate_position} F({reffile.upper()}):' not in line:
             line = reffile.readline()
         line = reffile.readline()
         for _ in range(n_elecs):
