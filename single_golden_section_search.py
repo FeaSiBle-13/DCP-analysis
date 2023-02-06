2e7a94f93f8580efe017bfd82f4b82fe88cd6331
Felix Bröcker <120687807+FeaSiBle-13@users.noreply.github.com>
2023-02-06 10:39:57 +0100

    Update single_golden_section_search.py

--- single_golden_section_search.py
+++ single_golden_section_search.py
@@ -5,20 +5,46 @@ import subprocess
 import numpy as np
 from pyscript import *
 
-method = 'newton'
-name = 'ethane'
-trajectory = int(input('which trajectory should be calculated?'))
-threshold_DCP_guess = 1e-1
 
-def reading_n_elecs():
+def read_n_elecs():
     with open(f'trajectory-1-max.ref', 'r') as reffile:
-        line = reffile.readline()
-        while 'MAX:' not in line:
-            line = reffile.readline()
-        line = reffile.readline()
-        words = line.split()
-        n_elecs = int(words[0])
+        for line in reffile:
+            if 'MAX:' in line:  
+                line = reffile.readline()
+                words = line.split()
+                n_elecs = int(words[0])
     return n_elecs
+
+
+def read_DCP_analysis_in(search):
+    with open('DCP_analysis.in', 'r') as reffile:
+        found = False
+        for line in reffile:
+            if search in line:
+                found = True
+                words = line.split()
+                try:
+                    float(words[1])
+                    is_float = True
+                except ValueError:
+                    is_float = False
+                if is_float:
+                    return(float(words[1]))
+                else:
+                    return(words[1])
+        #default values if not defined in .in file
+        if not found:
+            if search == 'threshold_electrons':
+                return(1e-3)
+            if search == 'threshold_molecule':
+                return(1e-1)
+            if search == 'method':
+                return('gradient_norm')
+            if search  == 'deflection_factor':
+                return(3e-3)
+            if search  == 'compare_mode':
+                return('electron_wise')
+
 
 
 def phi_value(trajectory):
@@ -54,6 +80,21 @@ def basin_outer_point(trajectory, basin):
     return np.array(R)
 
 
+def read_ref_file(reffile, coordinate_position):
+    temp = reffile.upper()
+    with open(f'trajectory-{trajectory}-{reffile}.ref', 'r') as reffile:
+        R = []
+        for line in reffile:
+            if f'{coordinate_position} F({temp}):' in line:
+                line = reffile.readline()
+                for _ in range(n_elecs):
+                    line = reffile.readline()
+                    words = line.split()
+                    for word in words:
+                        R.append(float(word))
+                return np.array(R)
+            
+            
 def basin_minimum(trajectory, basin):
     with open(f'./trajectory-{trajectory}-max.ref', 'r') as reffile:
         if basin == 'basin1':
@@ -73,11 +114,11 @@ def basin_minimum(trajectory, basin):
     return np.array(R)
 
 
-def DCP_guess_point(R_max1, R_max2, basin_outer_point, threshold_DCP_guess):
+def initial_guess_point(R_max1, R_max2, basin_outer_point, threshold_DCP_guess):
     R = []
     for l in range(n_elecs):
         norm = np.linalg.norm(R_max1[l*3:l*3+3]-R_max2[l*3:l*3+3])
-        if norm < threshold_DCP_guess:
+        if norm < threshold_electrons:
             for r in R_max2[l*3:l*3+3]:
                 R.append(r)
         else:
@@ -122,32 +163,14 @@ $maximize_sample()''')
             success = False
     if not success:
         print('Potential for golden section search could not be obtained')
-    
-
-#interpolation starts here
-n_elecs = reading_n_elecs()
-
-mkdir(f'trajectory-{trajectory}/temp')
-cp(f'{name}.wf', f'trajectory-{trajectory}/temp')
-
-start_pot1 =  DCP_guess_point(basin_minimum(trajectory, 'basin1'), basin_minimum(trajectory, 'basin2'), basin_outer_point(trajectory, 'basin1'), threshold_DCP_guess)
-start_pot2 =  DCP_guess_point(basin_minimum(trajectory, 'basin1'), basin_minimum(trajectory, 'basin2'), basin_outer_point(trajectory, 'basin2'), threshold_DCP_guess)
-
-a = start_pot1
-single_point_none(trajectory, n_elecs, a)
-print(phi_value(trajectory))
-
-b = start_pot2
-single_point_none(trajectory, n_elecs, b)
-print(phi_value(trajectory))
-
-
+        
+        
 def f(x, trajectory):
     single_point_none(trajectory, n_elecs, x)
     return(phi_value(trajectory))
 
 
-def gss(f, a, b, tol=1e-1):
+def golden_sec_search(f, a, b, tol=1e-1):
 
     c = b - (b - a) / gr
     d = a + (b - a) / gr
@@ -170,10 +193,39 @@ def gss(f, a, b, tol=1e-1):
         print(f'c = {c}')
         print(f'd = {d}\n')
     return ((b + a) / 2)
+    
 
+#script starts here
+#reads n_elecs from .max file and wavefunction from trajectory.ami
+n_elecs = read_n_elecs()
+name = read_trajectory_ami('file')
+
+threshold_electrons = read_DCP_analysis_in('threshold_electrons')
+method = read_DCP_analysis_in('method')
+
+#manual input of the trajectory
+trajectory = int(input('which trajectory should be calculated?'))
+
+#creates file structure
+mkdir(f'trajectory-{trajectory}/temp')
+cp(f'{name}.wf', f'trajectory-{trajectory}/temp')
+
+#makes the interpolation for the initial guess point
+a =  initial_guess_point(read_ref_file('max', 1), read_ref_file('max', 2), read_ref_file('traj', 1), threshold_DCP_guess)
+b =  initial_guess_point(read_ref_file('max', 1), read_ref_file('max', 2), read_ref_file('traj', 2), threshold_DCP_guess)
+
+#makes single point none calculation to obtain the potential
+single_point_none(trajectory, n_elecs, a)
+print(phi_value(trajectory))
+
+single_point_none(trajectory, n_elecs, b)
+print(phi_value(trajectory))
+
+#defines the golden ratio
 gr = (np.sqrt(5) + 1) / 2 
 
-R_new = gss(f, a, b)
+#makes the golden section search
+R_new = golden_sec_search(f, a, b)
 
 print(R_new)
 print(f(R_new, trajectory))
